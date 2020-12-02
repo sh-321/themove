8310fca93164d3c93b0fb470645d3e15228df4b0
sh-321 <tmdghksrhtn@naver.com>
2020-12-02 19:32:46 +0900

    1

---
+++ js/src/jeek.js
@@ -0,0 +1,43 @@
+(function($){
+    // 헤더 따라내려오는 기능
+    
+    // var header=$('.nav_pc');
+    // var headoffset1=header.offset().top;
+    // console.log(headoffset1);
+    
+    // $(window).on('scroll',function(e){
+    //     e.preventDefault();
+    //     var scroll=$(this).scrollTop();
+        
+    //     if(headoffset1<=scroll){
+    //         header.css({position:'fixed', margin:'auto', left:'50%', marginLeft:'-500px', top:0,zIndex:2000,bottom:'auto',opacity:0.8});}else
+    //         {header.css({position:'static',margin:'auto',opacity:1});}
+        
+      
+    
+    //     console.log(scroll);
+    //     console.log(headoffset1);
+    
+    // });
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+       
+    
+        
+    
+    
+    
+    
+    
+    })(jQuery);
